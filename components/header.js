442fab9ca2b0979b2c172e97c2f7c31c6f341162
Jude Bislig <judebislig@gmail.com>
2023-06-25 14:05:11 -0400

    removed search bar

--- components/header.js
+++ components/header.js
@@ -57,17 +57,6 @@ class Header extends HTMLElement {
             <a class="nav-link text-dark" href="#">Upcoming Events</a>
           </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input
-            class="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-            Search
-          </button>
-        </form>
       </div>
     </nav>
     `;
